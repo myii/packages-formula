7c51a4c3f0810d56caf3a2a097241f49c0fbbca2
Imran Iqbal <iqbalmy@hotmail.com>
2020-01-13 09:18:09 +0000

    fix(golang): ensure states run and tested for `ubuntu` only

--- test/integration/default/controls/golang_spec.rb
+++ test/integration/default/controls/golang_spec.rb
@@ -5,10 +5,10 @@ go_path = '/home/kitchen/go'
 # GO GET
 control 'go get github.com/golang/example/hello' do
   title 'should be installed'
-  desc '(only testing in the Debian platform, as these are OS-independent'
+  desc '(only testing in the Ubuntu platform, as these are OS-independent)'
 
   only_if do
-    os.debian?
+    platform[:name].ubuntu?
   end
 
   describe file('/usr/local/go/bin/go') do
